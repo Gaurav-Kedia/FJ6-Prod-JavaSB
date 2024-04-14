627e10eaa2787dd8c087d2efb4598d2d9b773c08
Gaurav kedia <Gaurav.kedia18@gmail.com>
2024-04-14 12:35:13 +0530

    feat: added scripts

---
+++ Scripts/Script.sh
@@ -0,0 +1,12 @@
+echo "********** Running Deploy Script .sh file **********"
+cd
+cd /home/ubuntu/Java-SB-1/FJ6-Prod-JavaSB/Scripts
+ls -l
+pwd
+echo "********** End of Deploy Script .sh file 1 **********"
+echo "********** End of Deploy Script .sh file 2 **********"
+echo "********** End of Deploy Script .sh file 3 **********"
+echo "********** End of Deploy Script .sh file 4 **********"
+echo "********** End of Deploy Script .sh file 5 **********"
+echo "********** End of Deploy Script .sh file 6 **********"
+echo "********** End of Deploy Script .sh file 7 **********"
