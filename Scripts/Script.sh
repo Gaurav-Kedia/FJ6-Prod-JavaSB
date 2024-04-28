f7d93d19c10e4acfcb73184cda7a757338dd5d38
Gaurav Kedia <gaurav.kedia18@Gmail.com>
2024-04-28 23:23:02 +0530

    Update Script.sh with tag ref, fetch, checkout, mvn install

--- Scripts/Script.sh
+++ Scripts/Script.sh
@@ -1,12 +1,32 @@
-echo "********** Running Deploy Script .sh file **********"
-cd
-cd /home/ubuntu/Java-SB-1/FJ6-Prod-JavaSB/Scripts
-ls -l
+#!/bin/bash
+
+echo "********** Running Script .sh file **********"
+
+# Change the directory to your SB project
+
+cd /home/ubuntu/Java-SB-1/FJ6-Prod-JavaSB
 pwd
+ls -lh
+
+# Receive tag reference as input parameter
+tag_ref="$1"
+
+if [ -z "$tag_ref" ]; then
+  echo "Error: Tag reference is empty. Exiting..."
+  exit 1
+fi
+
+# Fetch the latest changes from the remote repository
+git fetch
+
+# Checkout the specified tag
+git checkout "$tag_ref"
+
+echo "cleaning ang building with mvn"
+mvn clean install
+
+cd target
+echo "Inside Target folder"
+ls -lh
+
 echo "********** End of Deploy Script .sh file 1 **********"
-echo "********** End of Deploy Script .sh file 2 **********"
-echo "********** End of Deploy Script .sh file 3 **********"
-echo "********** End of Deploy Script .sh file 4 **********"
-echo "********** End of Deploy Script .sh file 5 **********"
-echo "********** End of Deploy Script .sh file 6 **********"
-echo "********** End of Deploy Script .sh file 7 **********"
